a4d57337634e665cc145a290d331d9f97c19014e
Marvin M <marvin23m@gmail.com>
2017-02-20 14:53:02 +0000

    Add association between person and phone number with test

--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -1,3 +1,5 @@
 class PhoneNumber < ApplicationRecord
+  belongs_to :person, optional: true
+
   validates :number, :person_id, presence: true
 end
--- spec/models/phone_number_spec.rb
+++ spec/models/phone_number_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe PhoneNumber, type: :model do
+
   let(:phone_number) { PhoneNumber.new(number: "1112223333", person_id: 1) }
 
   it 'is valid' do 
@@ -17,5 +18,9 @@ RSpec.describe PhoneNumber, type: :model do
     phone_number.person_id = nil
 
     expect(phone_number).not_to be_valid
+  end
+
+  it 'is associated with a person' do 
+    expect(phone_number).to respond_to(:person)
   end
 end

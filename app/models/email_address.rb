b677f12c5ebdf9472e6b5609aaf454d698a2c54c
Marvin M <marvin23m@gmail.com>
2017-02-22 13:50:38 +0000

    Test for validity

--- app/models/email_address.rb
+++ app/models/email_address.rb
@@ -1,3 +1,5 @@
 class EmailAddress < ApplicationRecord
   belongs_to :person, optional: true
+
+  validates :address, presence: true
 end
--- spec/controllers/email_addresses_controller_spec.rb
+++ spec/controllers/email_addresses_controller_spec.rb
@@ -24,11 +24,11 @@ RSpec.describe EmailAddressesController, type: :controller do
   # EmailAddress. As you add validations to EmailAddress, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    { address: 'email@email.com' }
   }
 
   let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
+    { address: nil }
   }
 
   # This should return the minimal set of values that should be in the session
@@ -102,15 +102,13 @@ RSpec.describe EmailAddressesController, type: :controller do
 
   describe "PUT #update" do
     context "with valid params" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
+      let(:new_attributes) {{ address: 'email@yahoo.com'}}
 
       it "updates the requested email_address" do
         email_address = EmailAddress.create! valid_attributes
         put :update, params: {id: email_address.to_param, email_address: new_attributes}, session: valid_session
         email_address.reload
-        skip("Add assertions for updated state")
+        expect(email_address.address).to eq('email@yahoo.com')
       end
 
       it "assigns the requested email_address as @email_address" do

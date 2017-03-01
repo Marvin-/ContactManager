db7d6942a35f12fb8cbb0d4f753c4d69da3ec707
Marvin M <marvin23m@gmail.com>
2017-03-01 12:37:58 +0000

    Add omniauth and a User model. Implement login UI.

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,3 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :twitter, "qh9dd5AiXXklINT2k1Xroolsn", "TeZu5L0vWrCKR2sYmvCdM21UrFs7sLesCOdcZMODfcWol68RcQ"
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,13 @@ Rails.application.routes.draw do
   resources :email_addresses
   resources :phone_numbers, :except => [:index, :show]
   resources :people
+
+  get '/auth/:provider/callback' => 'sessions#create'
+  root to: 'site#index'
+
+  resource :sessions, :only => [:create]
+  get "/login" => redirect("/auth/twitter"), as: :login
+  delete "/logout" => "sessions#destroy", as: :logout
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

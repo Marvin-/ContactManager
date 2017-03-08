3d5d5f8d3a37496fddd94d254b9e89b80c72f5af
Marvin M <marvin23m@gmail.com>
2017-03-08 08:21:33 +0000

    Refactor omniauth request

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,22 @@
 class SessionsController < ApplicationController
+
   def create
-    user = User.find_or_create_by_auth(request.env['omniauth.auth'])
+    user = User.find_or_create_by_auth(auth_hash)
     session[:user_id] = user.id
     redirect_to root_path, notice: "Logged in as #{user.name}"
   end
 
   def destroy
-    session[:user_id] = nil 
+    if current_user
+      session.delete(:user_id)
+      flash[:success] = 'See you!'
+    end
     redirect_to root_url, :notice => "Logged out!"
+  end
+
+  private
+  
+  def auth_hash
+    request.env['omniauth.auth']
   end
 end
